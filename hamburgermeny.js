7d814653556b20285e20648ea50993476ddc3a7c
AlbinTammemagi <146824168+AlbinTammemagi@users.noreply.github.com>
2023-11-16 09:32:55 +0100

    commit

--- hamburgermeny.js
+++ hamburgermeny.js
@@ -1,8 +1,8 @@
-console.log("Hamburger menu script loaded!");
-const menuToggle = document.getElementById("menu-toggle");
-const menu = document.getElementById("menu");
+    console.log("Hamburger menu script loaded!");
+    const menuToggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
 
-menuToggle.addEventListener("click", () => {
-    menu.classList.toggle("active");
-});
+    menuToggle.addEventListener("click", () => {
+        menu.classList.toggle("active");
+    });
 
